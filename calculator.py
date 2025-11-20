19138c3e9d18d45e2dca655b9259fd8b0eea329d
lukasgoofus <164433863+lukasgoofus@users.noreply.github.com>
2025-11-20 00:00:50 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,41 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/lukasgoofus/lab11-LV-PP.git
+# Partner 1: Lukas Vicovan
 
-One function per operation, in order.
+import math
+
 """
+calculator. py
+"""
+
 # First example
-def add(a, b): 
-    pass
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+
+def exp(a, b) :
+    return a**b
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 
